2055c98511446aa3d07369548f8b8147d55f1f64
PCWilson12 <117094166+PCWilson12@users.noreply.github.com>
2022-11-24 09:42:47 -0500

    Added pickup spawn logic to asteroidDamage
    A prefab still needs to be made and then attached to this script

--- Assets/Damage/asteroidDamage.cs
+++ Assets/Damage/asteroidDamage.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class asteroidDamage : MonoBehaviour
 {
-    
+
     // Start is called before the first frame update
+    GameObject pickup;
     void Start()
     {
         
@@ -13,6 +14,11 @@ public class asteroidDamage : MonoBehaviour
     public void OnCollisionEnter(Collision collision)
     {
         GameObject.Destroy(this.gameObject);
+       int pickupSpawn = Random.Range(1, 100);
+        if (pickupSpawn % 10 == 0)
+        {
+            GameObject.Instantiate(pickup, this.GetComponent<AsteroidMovement>().AsteroidRig.position, this.transform.rotation);
+        }
     }
 
     // Update is called once per frame
